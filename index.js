dd657bc56c51a91fcb28f13401b90ccbf2cf40ba
NickCoder123 <nikunjmehra935@gmail.com>
2023-07-09 19:35:28 +0530

    Cart functionality

---
+++ index.js
@@ -0,0 +1,60 @@
+import products from "./products.js";
+
+const cartContainer = document.querySelector("#cartContainer");
+const addToCartBtns = document.querySelectorAll(".addToCart");
+const refreshBtn = document.querySelector("#refreshBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
+
+const cart = [];
+
+const checkProduct = (id) => {
+  let product;
+
+  products.forEach((item) => {
+    if (item.id === id) {
+      product = item;
+    };
+  });
+
+  return product;
+};
+
+const addToCart = (id) => {
+  let itemForCart = checkProduct(id);
+
+  console.log(cart);
+  console.log(cart.length);
+
+  cart.push(itemForCart);
+
+  console.log('added');
+  console.log(itemForCart);
+}
+
+addToCartBtns.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    addToCart(`product-${index+1}`);
+  });
+});
+
+const updateCart = () => {
+  if (!cart) return;
+  cart.forEach((item) => {
+    if (cartContainer.innerHTML.includes(item.id)) return;
+    cartContainer.innerHTML += `
+      <div id="${item.id}" class="mt-9 mx-auto card md:w-4/6 sm:w-2/6 card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="${item.img}" alt="Shoes" class="cursor-pointer" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${item.desc}</h2>
+          <p class="font-semibold text-sky-500 text-xl">₹${item.price}</p>
+          <div class="card-actions justify-end">
+            <button class="btn addToCart"><span class="font-semibold orange_gradient">Add to Cart</span></button>
+          </div>
+        </div>
+      </div>
+    `
+  });
+};
+
+
+refreshBtn.addEventListener('click', updateCart);
---
+++ products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    "id": "product-1",
+    "desc": "Checked Shirt with Buttoned Flap Pockets",
+    "price": "699",
+    "img": "/images/products/product-1.webp",
+  },
+  {
+    "id": "product-2",
+    "desc": "Premium Stretch Slim Fit Shirt",
+    "price": "2999",
+    "img": "/images/products/product-2.avif",
+  },
+  {
+    "id": "product-3",
+    "desc": "All Day Pique Polo T-Shirt",
+    "price": "1399",
+    "img": "/images/products/product-3.avif",
+  },
+  {
+    "id": "product-4",
+    "desc": "Shirt with Concealed Button Placket",
+    "price": "699",
+    "img": "/images/products/product-4.avif",
+  },
+  {
+    "id": "product-5",
+    "desc": "Vintage Logo Heritage Crew-Neck T-Shirt",
+    "price": "488",
+    "img": "/images/products/product-5.avif",
+  },
+  {
+    "id": "product-6",
+    "desc": "Shirt with Cuffed Sleeves",
+    "price": "599",
+    "img": "/images/products/product-6.avif",
+  },
+];
+
+export default products
